ea2573d0789efb3eb20b730a5eca21b9192c3abd
aimldlprojects <136553456+aimldlprojects@users.noreply.github.com>
2024-01-25 18:22:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,31 +1,14 @@
-# Hardcoded credentials (replace these with your actual credentials)
-valid_username = "user123"
-valid_password = "password123"
-
-# Streamlit app layout
-def main():
-    st.title("Login Page")
-
-    # Sidebar for login
-    with st.sidebar:
-        login()
-
-# Login form
-def login():
-    st.subheader("Login")
-
-    # Input fields for username and password
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-
-    # Login button
-    if st.button("Login"):
-        if authenticate(username, password):
-            st.success("Login Successful!")
-            # Add redirection or other actions after successful login
-        else:
-            st.error("Invalid credentials. Please try again.")
-
-# Authentication function
-def authenticate(username, password):
-    return username == valid_username and password == valid_password
+Trans 
+	prompt example
+	multi lang support
+	- consistance in o/p
+		pre-processing
+			cleaning / removing null records
+			**filtering the specific that needs to be tanslated
+			inferencing the models with only the list of values 
+			inferencing the model with narrative included
+			
+	- o/p format
+		post processing
+			**fetching the list of values from response.
+			mapping the relevent fileds with values.
