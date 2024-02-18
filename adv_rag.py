a1d9dff83dbc620b79d2490ee09d1e54d4c23dce
aimldlprojects <136553456+aimldlprojects@users.noreply.github.com>
2024-02-18 10:19:05 +0530

    Update adv_rag.py

--- adv_rag.py
+++ adv_rag.py
@@ -3,5 +3,9 @@
 ref: https://github.com/sugarforever/Advanced-RAG/blob/main/04_langchain_per_user_retrieval.ipynb
 loader = PyPDFLoader("electronic-health-records.pdf")
 documents = loader.load_and_split()
+#add document per user in pine cone
 vectorstore.add_documents(documents, namespace=USER_1)
+retriever = vectorstore.as_retriever(search_type="similarity_score_threshold", search_kwargs={"namespace": USER_1, "score_threshold": .9})
+relevant_documents = retriever.get_relevant_documents(question)
+
 
