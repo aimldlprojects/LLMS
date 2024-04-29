2fa8edf6b154a7dd564561b6c919e40ab0273383
aimldlprojects <136553456+aimldlprojects@users.noreply.github.com>
2024-04-29 16:39:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-https://github.com/abidsaudagar/semantic-search-elastic-search-and-BERT-vector-embedding/blob/main/indexData.ipynb
+import pandas as pd
+import ast
+
+# Sample DataFrame
+data = {'vector_column': ["[1.111, 2.222]", "[3.333, 4.444]", "[5.555, 6.666]", "NaN", "[7.777, 8.888]"]}
+df = pd.DataFrame(data)
+
+# Convert string representations of lists to actual lists
+df['vector_column'] = df['vector_column'].apply(lambda x: ast.literal_eval(x) if pd.notna(x) else x)
+
+print(df)
