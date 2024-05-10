b1fc492fb50da1d2c59f2b45af417fe16c52a19e
aimldlprojects <136553456+aimldlprojects@users.noreply.github.com>
2024-05-10 18:21:23 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,41 +1,16 @@
-deploy:
-    needs: build
-    runs-on: self-hosted
-    steps:
-      - name: Pull UI Docker image
-        env:
-          ECR_REGISTRY: ***add ecr registry name***
-          ECR_REPOSITORY: ***add ecr repository name***
-          IMAGE_TAG: ${{ github.sha }}    
-        run: |
-          docker pull $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
-          echo "UI Docker image is pulled from $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG"
+- name: List Docker containers
+      run: docker container ls -a
  
-      - name: Delete Old docker container
-        run: |
-          docker rm -f ***add a container name*** || true
-          echo "Deleted old UI container"
+   - name: Stop and remove Docker containers
+      run: |
+        docker stop $(docker ps -aq)
+        docker rm $(docker ps -aq)    
  
-      - name: Run Docker Container
-        run: |
-          sudo docker run -d -p 3015:80 --name ***add a container name*** $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
-          echo "Running UI container"
-     
-      - name: Pull App Docker image
-        env:
-          ECR_REGISTRY: ***add ecr registry name***
-          ECR_REPOSITORY: ***add ecr repository name***
-          IMAGE_TAG: ${{ github.sha }}    
-        run: |
-          docker pull $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
-          echo "App Docker image is pulled from $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG"
+   - name: List Docker images
+      run: docker image ls
  
-      - name: Delete Old docker container
-        run: |
-          docker rm -f ***add a container name*** || true
-          echo "Deleted old App container"
- 
-      - name: Run Docker Container
-        run: |
-          sudo docker run -d --name ***add a container name*** -p 8015:8015  $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
-          echo "Running App2 container"
+    - name: Delete Docker images
+      run: |
+        docker rmi -f $(docker images -aq)
+    - name: List Docker images after deletion
+      run: docker image ls
